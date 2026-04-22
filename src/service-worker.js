6f22783ffd87fa30172053099dadbcc5f572a3a2
Jaya <your_email@example.com>
2026-04-22 12:50:04 +0530

    PWA work final modification

---
+++ src/service-worker.js
@@ -0,0 +1,56 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+import { build, files, version } from '$service-worker';
+
+const CACHE_NAME = `brahmi-cache-${version}`;
+const ASSETS = [...build, ...files];
+
+self.addEventListener('install', (event) => {
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE_NAME);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener('activate', (event) => {
+	async function deleteOldCaches() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE_NAME) await caches.delete(key);
+		}
+	}
+
+	event.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener('fetch', (event) => {
+	if (event.request.method !== 'GET') return;
+
+	async function respond() {
+		const url = new URL(event.request.url);
+		const cache = await caches.open(CACHE_NAME);
+
+		if (ASSETS.includes(url.pathname)) {
+			const response = await cache.match(url.pathname);
+			if (response) return response;
+		}
+
+		try {
+			const response = await fetch(event.request);
+			if (response.status === 200) {
+				cache.put(event.request, response.clone());
+			}
+			return response;
+		} catch {
+			const cachedResponse = await cache.match(event.request);
+			if (cachedResponse) return cachedResponse;
+			throw new Error('Network unavailable and no cache match');
+		}
+	}
+
+	event.respondWith(respond());
+});
